cafe702bb82ac2f1190b36528df06ba0ffb7acd1
maslabgamer <maslabgamer@gmail.com>
2015-05-25 23:09:13 -0700

    Fixed some issues
    - Removed extraneous lines
    - Commands now send after ping properly

--- MasBotv02.py
+++ MasBotv02.py
@@ -19,7 +19,6 @@ class IRC_Server:
         self.irc_current_channel = ""
         self.irc_sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
         self.is_connected = False
-        self.should_reconnect = False
         self.owner = OWNER
         self.command = ""
         self.param = ""
@@ -28,13 +27,12 @@ class IRC_Server:
         self.lFirstWord = ""
         self.UserList = {}
         self.Commands = {'roll': Dice_Roller.parse, 'test': Test_Module.call, 'tell': self.store_message}
-        self.response = {'belgium': [1, 'gasps!'], 'kicks masbot': [2, 'Rude.']}
+        self.response = {'belgium': [1, 'gasps!'], 'kicks masbot': [2, 'Rude.'], 'boops masbot': [1, 'boops back!']}
         
     def resCom(self):
         self.Commands = {'roll': Dice_Roller.parse, 'test': Test_Module.call, 'tell': self.store_message}
         
     def connect(self):
-        self.should_reconnect = True
         self.irc_sock.connect((self.irc_host, self.irc_port))
         print ("Connected to: {0}:{1}".format(str(self.irc_host), str(self.irc_port)))
         self.irc_sock.send (("NICK {0} \r\n".format(self.irc_nick)).encode())
@@ -60,8 +58,9 @@ class IRC_Server:
         for i, v in enumerate(l):
             v = v if v[0].isalpha() else v[1:]
             l[i] = v if '\r\n' not in v[-2:] else v[:-2]
-        l.remove('MasBot')
+        if 'MasBot' in l: l.remove('MasBot')
         self.UserList[self.irc_current_channel] = l
+        print "Userlist is {0}".format(self.UserList)
         
     def getInfo(self, l):
         l = l.split(':', 2)
@@ -74,9 +73,6 @@ class IRC_Server:
         if self.lFirstWord[0] == '.': 
             self.command = self.lFirstWord[1:]
         else: self.command = " "
-        
-    def makeReadable(self, l):
-        return "{0}: {1}".format(self.lName, self.lText)
         
     def rel(self, t):
         mod = sys.modules.get(t.split(' ')[1].rstrip()) 
@@ -96,15 +92,13 @@ class IRC_Server:
             days = t[0].split(',')
             days[1] = days[1].strip()
             t[0] = days[1]
-            days = int(days[0].split(' ')[0])
-            if days > 0: timeM += "{0} days, ".format(days)
+            if int(days[0].split(' ')[0]) > 0: timeM += "{0} days, ".format(days)
         for i, v in enumerate(t):
             t[i] = int(v)
         if t[0] > 0: timeM += "{0} hours, ".format(t[0])
         if t[1] > 0: timeM += "{0} minutes, ".format(t[1])
         if len(timeM) > 0: timeM += "and "
-        timeM += "{0} seconds ago.".format((t[2]))
-        return timeM
+        return timeM + "{0} seconds ago.".format((t[2]))
         
     def messageCheck(self):
         db_cursor.execute(select_message, (self.lName,) )
@@ -122,17 +116,16 @@ class IRC_Server:
         while connected:
             line = self.irc_sock.recv(4096) #recieve server messages
             self.getInfo(line)
+            
             self.messageCheck()
             if 'PING' in line: # Call a parsingfunction
-                self.irc_sock.send('PONG {0}n'.format(line.rstrip().split()[1]))
+                self.irc_sock.send('PONG {0}\r\n'.format(line.rstrip().split()[1]))
             elif self.command.strip() in self.Commands:
                 self.send_message(self.Commands[self.command.strip()](self.lText.split(' ', 1)[1] if len(self.lText.split(' ', 1)) > 1 else ""))
             elif self.command == 'reload': self.rel(self.lText)
             elif '353' in line: self.getUsers(line)
-            elif 'JOIN' in line: self.getUserlist(self.irc_current_channel)
-            self.command = " "
-            self.lFirstWord = " "
-            print (self.makeReadable(line) if 'PRIVMSG' in line else line)
+            elif 'JOIN' in line or 'PART' in line: self.getUserlist(self.irc_current_channel)
+            print ("{0}: {1}".format(self.lName, self.lText) if 'PRIVMSG' in line else line)
             self.choose_response()
         
 HOST="irc.sorcery.net"
